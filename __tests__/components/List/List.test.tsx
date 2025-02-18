2b7e8ecae6782c0329814f8e8c152bd03225e699
Andres Alvarez <andresalvarez0397@outlook.com>
2025-02-18 16:12:56 -0500

    adjust components imports paths in tests

--- __tests__/components/List/List.test.tsx
+++ __tests__/components/List/List.test.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { List } from 'src/components/List/List'; // Adjust the import path as necessary
+import { List } from '@/src/components/List/List';
 
 // Mock data for testing
 const mockTasks = [
